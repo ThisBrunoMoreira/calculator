d0d5cb3cdbbd239d5d30c9b0f43a48bb1807f98c
ThisBrunoMoreira <124924557+ThisBrunoMoreira@users.noreply.github.com>
2023-03-05 08:15:24 -0300

    feat: Add support for drag events on calculator buttons
    - Add addEventListenerAll method to add event listeners to multiple events
    - Update initButtonsEvents method to add drag event listeners to buttons

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,7 @@
 class CalcController {
     constructor() {
         this._locale = 'pt-BR'
-        
+
         this._dateEl = document.querySelector('[data-day]')
         this._timeEl = document.querySelector('[data-hour]')
         this._displayCalcEl = document.querySelector('[data-display]')
@@ -13,9 +13,11 @@ class CalcController {
     }
 
     /*
-      initialize
+  
       Inicializa a exibição da data e hora atualizável na calculadora e atualiza a cada segundo.
-      @returns {void}
+        @function
+        @name initialize
+        @returns {void}
     */
     initialize() {
         this.setDisplayDateTime();
@@ -25,10 +27,12 @@ class CalcController {
     }
 
     /*
-      setDisplayDateTime
+      
       Define a data e a hora atual para exibição na calculadora.
      
-      @returns {void}
+        @function
+        @name setDisplayDateTime
+        @returns {void}
     */
     setDisplayDateTime() {
         this.displayDate =
@@ -37,15 +41,45 @@ class CalcController {
             this.currentDate.toLocaleTimeString(this._locale);
     }
 
-    initButtonsEvents() {
 
-       this._buttons.forEach((btn, index) => {
-            btn.addEventListener('click', e=> {
-                console.log(btn.dataset.btn)
-            })
-       })
+    /*
+
+     Adiciona ouvintes de eventos a um elemento para vários eventos.
+    
+        Este método adiciona ouvintes de eventos a um elemento para vários eventos especificados em uma string separada por espaços.
+        Os ouvintes de eventos são adicionados usando o método forEach do array de eventos.
+    
+        @function
+        @name addEventListenerAll
+        @param {Element} element - O elemento ao qual os ouvintes de eventos serão adicionados.
+        @param {string} events - Uma string contendo os nomes dos eventos separados por espaços.
+        @param {function} fn - A função que será executada quando qualquer um dos eventos for acionado.
+    */
+    addEventListenerAll(element, events, fn) {
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn)
+        })
+    }
+    /*
+        Inicializa os ouvintes de eventos de clique e arrasto para os botões da calculadora.
+        Este método inicializa os ouvintes de eventos de clique e arrasto para todos os botões com o atributo 'data-btn'.
+        Os botões são obtidos usando um seletor de consulta e armazenados como uma propriedade da classe para fácil acesso.
+        Os ouvintes de eventos são adicionados usando o método forEach do array de botões e o método addEventListenerAll desta classe,
+        e registram o conjunto de dados do botão no console quando clicado ou arrastado.
+  
+        @function
+        @name initButtonsEvents
+  */
+    initButtonsEvents() {
+        const buttons = this._buttons;
+        buttons.forEach((btn) => {
+            this.addEventListenerAll(btn, 'click drag', () => {
+                console.log(btn.dataset.btn);
+            });
+        });
     }
 
+ 
 
     get displayTime() {
         return this._timeEl.innerHTML = time
